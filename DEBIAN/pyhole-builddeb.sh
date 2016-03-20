d0395931898c14999ddaa342ddc84509c46c5fdd
ryt51V <ryt51V>
2016-03-20 10:06:00 +0000

    Static files moved to etc folder where they are installed as part of the package, rather than copied or created by script. - cron.d/pyhole - sudoers.d/pyhole - pyhole/pyhole.logrotate
    pyhole-config:
    - sudoers file: Just set permissions rather than install.
    - cron file: Do nothing in script.

--- DEBIAN/pyhole-builddeb.sh
+++ DEBIAN/pyhole-builddeb.sh
@@ -3,26 +3,31 @@
 thisdir=$(dirname "$0")
 tmpdir=$(mktemp --directory)
 
+# /DEBIAN
 mkdir -p "$tmpdir/DEBIAN"
 touch "$tmpdir/DEBIAN/.keep"
 cp -R "$thisdir/"* "$tmpdir/DEBIAN"
 
+# /usr/bin
 mkdir -p "$tmpdir/usr/bin"
-touch "$tmpdir/usr/bin/.keep"
 cp -R "$thisdir/../bin/"* "$tmpdir/usr/bin"
 
+# /etc & /etc/pyhole
 mkdir -p "$tmpdir/etc/pyhole"
 touch "$tmpdir/etc/pyhole/.keep"
+cp -R "$thisdir/../etc/"* "$tmpdir/etc"
 
+# /usr/share/pyhole
 mkdir -p "$tmpdir/usr/share/pyhole"
 touch "$tmpdir/usr/share/pyhole/.keep"
 cp -R "$thisdir/../files/"* "$tmpdir/usr/share/pyhole"
 
-mkdir -p "$tmpdir/usr/lib/python3.4"
-touch "$tmpdir/usr/lib/python3.4/.keep"
-cp -R "$thisdir/../lib/pyhole" "$tmpdir/usr/lib/python3.4/pyhole"
+# /usr/lib/python3.4/pyhole
+mkdir -p "$tmpdir/usr/lib/python3.4/pyhole"
+touch "$tmpdir/usr/lib/python3.4/pyhole/.keep"
+cp -R "$thisdir/../lib/pyhole/"* "$tmpdir/usr/lib/python3.4/pyhole"
 
-
+# /var/lib/pyhole
 mkdir -p "$tmpdir/var/lib/pyhole"
 touch "$tmpdir/var/lib/.keep"
 
